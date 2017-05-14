a9095adfd23ae3663229c766cbf383e803556f2e
Adhita Selvaraj <Adhita@Adhita-MacBook-Air.local>
2017-05-14 00:21:03 -0400

    Made exclusively Flask

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
-main = Blueprint('main', __name__)
+import os, time, sys
 
 import json
 from engine import RecommendationEngine
@@ -9,20 +9,23 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 from flask import Flask, request
+game = Flask(__name__)
 
-@main.route("/<user_id>/ratings/top/10/<int:sentiment>", methods=["GET"])
+from pyspark import SparkContext, SparkConf
+
+@game.route("/<user_id>/ratings/top/10/<int:sentiment>", methods=["GET"])
 def top_ratings(user_id, sentiment):
 	logger.debug("User %s TOP ratings requested", user_id)
 	top_ratings = recommendation_engine.get_top_ratings(user_id, 10, sentiment)
 	return json.dumps(top_ratings)
 
-@main.route("/<int:user_id>/ratings/<int:movie_id>", methods=["GET"])
+@game.route("/<int:user_id>/ratings/<int:movie_id>", methods=["GET"])
 def movie_ratings(user_id, movie_id):
 	logger.debug("User %s rating requested for movie %s", user_id, movie_id)
 	ratings = recommendation_engine.get_ratings_for_movie_ids(user_id, [movie_id])
 	return json.dumps(ratings)
 
-@main.route("/<int:user_id>/ratings", methods=["POST"])
+@game.route("/<int:user_id>/ratings", methods=["POST"])
 def add_ratings(user_id):
 	ratings_list = request.form.keys()[0].strip().split("\n")
 	ratings_list = map(lambda x: x.split(","), ratings_list)
@@ -31,13 +34,10 @@ def add_ratings(user_id):
 
 	return json.dumps(ratings)
 
-
-def create_app(spark_context, dataset_path):
+if __name__ == '__main__':
 	global recommendation_engine
-
+	conf = SparkConf().setAppName("movie-server")
+	spark_context = SparkContext(conf=conf, pyFiles=['engine.py'])
+	dataset_path = os.getcwd()
 	recommendation_engine = RecommendationEngine(spark_context, dataset_path)
-
-
-	app = Flask(__name__)
-	app.register_blueprint(main)
-	return app
+	game.run(host='0.0.0.0', port=8080)
